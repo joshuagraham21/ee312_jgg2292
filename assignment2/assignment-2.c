05da311890f29ae1b935d2d611deccd28880d6b7
joshuagraham21 <joshua.graham@utexas.edu>
2016-09-15 23:53:49 -0500

    Update assignment-2.c

--- assignment2/assignment-2.c
+++ assignment2/assignment-2.c
@@ -1,4 +1,4 @@
-// assignment2.cpp : Defines the entry point for the console application.
+// assignment2.c : Defines the entry point for the console application.
 //
 #include <stdio.h>
 #include "stdafx.h"
